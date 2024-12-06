5f1ab2969ddd65af4ea6859fe58e2bd9da655426
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-12-06 22:58:45 +0100

    2 commit

---
+++ src/main/java/train/shp4k/Shp4kApplication.java
@@ -0,0 +1,13 @@
+package train.shp4k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shp4kApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shp4kApplication.class, args);
+	}
+
+}
---
+++ src/test/java/train/shp4k/Shp4kApplicationTests.java
@@ -0,0 +1,13 @@
+package train.shp4k;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shp4kApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
