f429d8fcd05ddaf446ddb4e27ff83ba08794b0c1
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-12-07 01:09:16 +0100

    ProductDto

---
+++ src/main/java/train/shp4k/domain/dto/ProductDto.java
@@ -0,0 +1,84 @@
+package train.shp4k.domain.dto;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * 07/12/2024 shp4k
+ *
+ * @author Boris Iurciuc (cohort36)
+ */
+public class ProductDto {
+
+  private Long id;
+  private String title;
+  private String description;
+  private BigDecimal price;
+  private String image;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof ProductDto that)) {
+      return false;
+    }
+    return Objects.equals(getId(), that.getId()) && Objects.equals(getTitle(),
+        that.getTitle()) && Objects.equals(getDescription(), that.getDescription())
+        && Objects.equals(getPrice(), that.getPrice()) && Objects.equals(
+        getImage(), that.getImage());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getTitle(), getDescription(), getPrice(), getImage());
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Product: id - %d, title - %s, price - %s, description - %s",
+        id, title, price, description);
+  }
+
+}
