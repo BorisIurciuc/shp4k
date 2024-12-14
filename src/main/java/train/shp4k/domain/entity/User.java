9d268a8d5bc5df6c397d673cbac750fbdfb97504
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-12-14 19:00:10 +0100

    table & class User

---
+++ src/main/java/train/shp4k/domain/entity/User.java
@@ -0,0 +1,111 @@
+package train.shp4k.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Objects;
+
+/**
+ * 14/12/2024 shp4k
+ *
+ * @author Boris Iurciuc (cohort36)
+ */
+@Entity
+@Table
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", unique = true, nullable = false)
+  private Long id;
+
+  @Column(name = "username", unique = true, nullable = false)
+  private String username;
+
+  @Column(name = "password", nullable = false)
+  private String password;
+
+  @Column(name = "email", unique = true, nullable = false)
+  private String email;
+
+  @Column(name = "active",  nullable = false)
+  private boolean active;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof User user)) {
+      return false;
+    }
+    return isActive() == user.isActive() && Objects.equals(getId(), user.getId())
+        && Objects.equals(getUsername(), user.getUsername()) && Objects.equals(
+        getPassword(), user.getPassword()) && Objects.equals(getEmail(), user.getEmail());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getUsername(), getPassword(), getEmail(), isActive());
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+        "id=" + id +
+        ", username='" + username + '\'' +
+        ", password='" + password + '\'' +
+        ", email='" + email + '\'' +
+        ", active=" + active +
+        '}';
+  }
+
+  //     Метод для получения зашифрованного пароля
+//     для добавления пользователей в БД вручную
+  public static void main(String[] args) {
+    System.out.println(new BCryptPasswordEncoder().encode("111"));
+  }
+}
